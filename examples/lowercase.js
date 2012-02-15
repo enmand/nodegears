c5ae03cdc420ba31b0cc082165679f7756fcc7f2
Daniel Enman <enmand@unerror.com>
2012-02-15 08:57:37 -0400

    Added examples for Gearman workers

---
+++ examples/lowercase.js
@@ -0,0 +1,12 @@
+var nodegears = require('../nodegears.js');
+
+_client = new nodegears.Client();
+_client.submit('lowerCase', 'WHAT A GREAT LIBRARY!', "normal", false, function(data){
+	console.log(data.toString('utf8'));
+});
+
+_worker = new nodegears.Worker();
+_worker.register('lowerCase', function(_workload)
+{
+	return _workload.toLowerCase();
+});
